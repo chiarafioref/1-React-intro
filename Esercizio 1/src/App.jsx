0d3802f68d3a81ae393e878227b06c33930ccf88
Chiara <chiarafioref@gmail.com>
2026-07-03 17:41:47 +0200

    first commit

---
+++ Esercizio 1/src/App.jsx
@@ -0,0 +1,12 @@
+import Image from './components/Image'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Image />
+  )
+}
+
+export default App
---
+++ Esercizio 1/src/components/Image.jsx
@@ -0,0 +1,8 @@
+let image = "https://tse2.mm.bing.net/th/id/OIP.QrxW939zj1L8e2EzY_oIhQHaFQ?pid=Api&P=0&h=180";
+
+function Image() {
+    return (
+        <img src={image} />
+    )
+}
+export default Image;
